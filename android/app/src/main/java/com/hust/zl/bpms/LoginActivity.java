2c317358603e0bd6555215ca34a8efe470804920
bluebloodsky <zhangliang1101@126.com>
2018-07-30 17:55:52 +0800

    android learning

---
+++ android/app/src/main/java/com/hust/zl/bpms/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.hust.zl.bpms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.litepal.tablemanager.Connector;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
