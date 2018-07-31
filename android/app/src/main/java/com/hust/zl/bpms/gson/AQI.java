804d28f7d6cbb771d40b5f13b59aba9cb38b0c84
bluebloodsky <zhangliang1101@126.com>
2018-07-31 17:18:10 +0800

    learning ...

---
+++ android/app/src/main/java/com/hust/zl/bpms/gson/AQI.java
@@ -0,0 +1,9 @@
+package com.hust.zl.bpms.gson;
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ android/app/src/main/java/com/hust/zl/bpms/util/Utility.java
@@ -0,0 +1,75 @@
+package com.hust.zl.bpms.util;
+
+import android.text.TextUtils;
+
+import com.hust.zl.bpms.db.City;
+import com.hust.zl.bpms.db.County;
+import com.hust.zl.bpms.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utility {
+    public static boolean handleProvinceResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0; i < allProvinces.length(); i++) {
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCityResponse(String response,int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i= 0; i<allCities.length();i++){
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCountyResponse(String response,int cityId){
+        if(!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCounties = new JSONArray(response);
+                for (int i= 0; i<allCounties.length();i++){
+                    JSONObject coutyObject= allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(coutyObject.getString("name"));
+                    county.setWeatherId(coutyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+
+
+}
